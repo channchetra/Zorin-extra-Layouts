3fd613f812c4c489f25181adbeb14f07b0d43d90
TGRush <simeonlps@protonmail.com>
2022-01-11 01:40:01 +0100

    mmm
    Signed-off-by: TGRush <simeonlps@protonmail.com>

--- README.md
+++ README.md
@@ -2,19 +2,14 @@
 ## A script which allows anyone to easily switch between some of Zorin's Pro/Ultimate Layouts on Core editions (GNOME, not Lite/XFCE)
 ### Note: I WILL take this down if the Zorin developers should contact me on that, so don't expect this to stay forever
 
-This script currently covers the **MacOS-Like Layout**, others will be added as I become less lazy (or learn how to implement things better, or others contribute, etc)
+This script currently covers the **MacOS-Like Layout**, others will be added as I become less lazy
 
-_**Note that I'm learning how to use bash in scripts through this, so many things can, and likely will be improved (a lot) as I learn**_
+---
 
+## How To Use
+Run this:
+```
+N/A
+```
 
-## How to use:
-first, download/clone the repository:
-```
-git clone https://github.com/TGRush/Zorin-extra-Layouts.git
-```
-
-then simply run the script like this:
-```
-chmod +x ./morelayoutsinstall.sh && ./morelayoutsinstall.sh
-```
-The rest is self-explanatory
+Done.
--- morelayoutsinstall.sh
+++ morelayoutsinstall.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
+
+# Ansi color code variables
+# shellcheck disable=SC2034
+red="\e[0;91m"
+# shellcheck disable=SC2034
+blue="\e[0;94m"
+# shellcheck disable=SC2034
+green="\e[0;92m"
+# shellcheck disable=SC2034
+white="\e[0;97m"
+# shellcheck disable=SC2034
+bold="\e[1m"
+# shellcheck disable=SC2034
+uline="\e[4m"
+# shellcheck disable=SC2034
+reset="\e[0m"
+
 # this is just because Sudo decided to not accept just running zenity for the password prompt in a way that I liked, aka I'm imcompetent as hell
-export SUDO_ASKPASS=./zenitypass.sh
 
-
-ask=`zenity --list --title="Installation Options" --column="0" "MacOS-Layout" "Ubuntu-Layout" --width=100 --height=300 --hide-header`
-
+if [ $(ls /usr/bin/zenity) == "/usr/bin/zenity" ]
+then
+export SUDO_ASKPASS=$(zenity --password)
+ask=$(zenity --list --title="Installation Options" --column="0" "MacOS-Layout" "Ubuntu-Layout" --width=100 --height=300 --hide-header)
 if [ "$ask" == "MacOS-Layout" ]; then
-	sudo -A apt install gnome-shell-extension-zorin-dash gnome-shell-extension-zorin-hide-activities-move-clock -y
-	echo "\n"
-	echo '#############################'
-	echo "\n" >> ~/.profile
-	echo 'gnome-extensions enable zorin-dash@zorinos.com' >> ~/.profile
-	echo 'awk -v n=2 'NR==FNR{total=NR;next} FNR==total-n+1{exit} 1' ~/.profile ~/.profile >> temp.txt && mv temp.txt ~/.profile'
-	echo rm temp.txt
-	echo '# Installed, log out and log back in to properly apply!'
+	sudo apt install gnome-shell-extension-zorin-dash gnome-shell-extension-zorin-hide-activities-move-clock -y
+	echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
+	sleep 5s
+	busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
+	echo "waiting 10s..."
+	sleep 10s
+	gnome-extensions disable zorin-menu@zorinos.com
+	gnome-extensions disable zorin-taskbar@zorinos.com
+	gnome-extensions enable zorin-dash@zorinos.com
+	gnome-extensions enable zorin-hide-activities-move-clock@zorinos.com
+	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
 fi
 
 if [ "$ask" == "Ubuntu-Layout" ]; then
-	sudo -A apt install gnome-shell-extension-zorin-dash
-# 	sudo -A mv
+	echo -e "${red}Not Supported Yet!${reset}"
+	# sudo -A apt install gnome-shell-extension-zorin-dash
+	
 fi
+else
+echo -e "${red}Zenity not found! run sudo apt install zenity first!${reset}"
 exit 0
+fi
