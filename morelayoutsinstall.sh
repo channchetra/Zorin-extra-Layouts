7408d168a7d7eabaa8db7530b6d80ea4d35f7e23
TGRush <simeonlps@protonmail.com>
2022-01-11 18:28:18 +0100

    add a "other OS" placeholder

--- morelayoutsinstall.sh
+++ morelayoutsinstall.sh
@@ -18,58 +18,62 @@ reset="\e[0m"
 
 # this is just because Sudo decided to not accept just running zenity for the password prompt in a way that I liked, aka I'm imcompetent as hell
 
-if [ "$(ls /usr/bin/zenity)" == "/usr/bin/zenity" ]
-then
-ask=$(zenity --list --title="Installation Options" --column="0" "MacOS-Layout" "Ubuntu-Layout" "Windows Classic-Layout" "Windows 11-Layout" --width=100 --height=300 --hide-header)
-if [ "$ask" == "MacOS-Layout" ]; then
-	
-	sudo apt install gnome-shell-extension-zorin-dash gnome-shell-extension-zorin-hide-activities-move-clock -y
-	echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
-	sleep 5s
-	busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
-	echo "waiting 10s..."
-	sleep 10s
-	gnome-extensions disable zorin-menu@zorinos.com
-	gnome-extensions disable zorin-taskbar@zorinos.com
-	gnome-extensions enable zorin-dash@zorinos.com
-	gnome-extensions enable zorin-hide-activities-move-clock@zorinos.com
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
-fi
+if [ "$(ls /usr/bin/zenity)" == "/usr/bin/zenity" ]; then
+	ask=$(zenity --list --title="Installation Options" --column="0" "MacOS-Layout" "Ubuntu-Layout" "Windows Classic-Layout" "Windows 11-Layout" --width=100 --height=300 --hide-header)
+	if [ "$ask" == "MacOS-Layout" ]; then
 
-if [ "$ask" == "Ubuntu-Layout" ]; then
-	echo -e "${red}Make sure to fill in your password in the Terminal! there is no popup.${reset}"
-	sudo apt install gnome-shell-extension-zorin-dash -y
-	echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
-	busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting...")'
-	echo "waiting 10s..."
-	sleep 10s
-	gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
-	gnome-extensions disable zorin-menu@zorinos.com
-	gnome-extensions disable zorin-taskbar@zorinos.com
-	gnome-extensions enable zorin-dash@zorinos.com
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/ubuntu-zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
-fi
+		sudo apt install gnome-shell-extension-zorin-dash gnome-shell-extension-zorin-hide-activities-move-clock -y
+		echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
+		sleep 5s
+		busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
+		echo "waiting 10s..."
+		sleep 10s
+		gnome-extensions disable zorin-menu@zorinos.com
+		gnome-extensions disable zorin-taskbar@zorinos.com
+		gnome-extensions enable zorin-dash@zorinos.com
+		gnome-extensions enable zorin-hide-activities-move-clock@zorinos.com
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
+	fi
 
-if [ "$ask" == "Windows Classic-Layout" ]; then
-	echo -e "${red}Note that Gnome won't be restarted for this layout.${reset}"
-	gnome-extensions disable zorin-dash@zorinos.com
-	gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
-	gnome-extensions enable zorin-taskbar@zorinos.com
-	gnome-extensions enable zorin-menu@zorinos.com
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/classic-panel-conf | dconf load /org/gnome/shell/extensions/zorin-taskbar/ 
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/classic-menu-conf | dconf load /org/gnome/shell/extensions/zorin-menu/
-fi
+	if [ "$ask" == "Ubuntu-Layout" ]; then
+		echo -e "${red}Make sure to fill in your password in the Terminal! there is no popup.${reset}"
+		sudo apt install gnome-shell-extension-zorin-dash -y
+		echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
+		busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting...")'
+		echo "waiting 10s..."
+		sleep 10s
+		gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
+		gnome-extensions disable zorin-menu@zorinos.com
+		gnome-extensions disable zorin-taskbar@zorinos.com
+		gnome-extensions enable zorin-dash@zorinos.com
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/ubuntu-zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
+	fi
 
-if [ "$ask" == "Windows 11-Layout" ]; then
-	echo -e "${red}Note that Gnome won't be restarted for this layout.${reset}"
-	gnome-extensions disable zorin-dash@zorinos.com
-	gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
-	gnome-extensions enable zorin-taskbar@zorinos.com
-	gnome-extensions enable zorin-menu@zorinos.com
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/11-panel-conf | dconf load /org/gnome/shell/extensions/zorin-taskbar/
-	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/11-menu-conf | dconf load /org/gnome/shell/extensions/zorin-menu/
-fi
+	if [ "$ask" == "Windows Classic-Layout" ]; then
+		echo -e "${red}Note that Gnome won't be restarted for this layout.${reset}"
+		gnome-extensions disable zorin-dash@zorinos.com
+		gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
+		gnome-extensions enable zorin-taskbar@zorinos.com
+		gnome-extensions enable zorin-menu@zorinos.com
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/classic-panel-conf | dconf load /org/gnome/shell/extensions/zorin-taskbar/
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/classic-menu-conf | dconf load /org/gnome/shell/extensions/zorin-menu/
+	fi
+
+	if [ "$ask" == "Windows 11-Layout" ]; then
+		echo -e "${red}Note that Gnome won't be restarted for this layout.${reset}"
+		gnome-extensions disable zorin-dash@zorinos.com
+		gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
+		gnome-extensions enable zorin-taskbar@zorinos.com
+		gnome-extensions enable zorin-menu@zorinos.com
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/11-panel-conf | dconf load /org/gnome/shell/extensions/zorin-taskbar/
+		curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/11-menu-conf | dconf load /org/gnome/shell/extensions/zorin-menu/
+	fi
+
+	if [ "$ask" == "Other OS" ]; then
+		echo -e "${red}this is just a placeholder${reset}"
+	fi
 else
-echo -e "${red}Zenity not found or something else went wrong! run sudo apt install zenity first and try again!${reset}"
-exit 0
+
+	echo -e "${red}Zenity not found or something else went wrong! run sudo apt install zenity first and try again!${reset}"
+	exit 0
 fi
