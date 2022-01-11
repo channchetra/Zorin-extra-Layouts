874cf289908cb7ded47b58bc826ea0f7f185460a
TGRush <simeonlps@protonmail.com>
2022-01-11 14:25:00 +0100

    add ubuntu layout
    Signed-off-by: TGRush <simeonlps@protonmail.com>

--- morelayoutsinstall.sh
+++ morelayoutsinstall.sh
@@ -22,7 +22,7 @@ if [ $(ls /usr/bin/zenity) == "/usr/bin/zenity" ]
 then
 ask=$(zenity --list --title="Installation Options" --column="0" "MacOS-Layout" "Ubuntu-Layout" --width=100 --height=300 --hide-header)
 if [ "$ask" == "MacOS-Layout" ]; then
-	echo -e "${red}Make sure to fill in your password in the Terminal! there is no popup.${reset}"
+	
 	sudo apt install gnome-shell-extension-zorin-dash gnome-shell-extension-zorin-hide-activities-move-clock -y
 	echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
 	sleep 5s
@@ -37,9 +37,17 @@ if [ "$ask" == "MacOS-Layout" ]; then
 fi
 
 if [ "$ask" == "Ubuntu-Layout" ]; then
-	echo -e "${red}Not Supported Yet!${reset}"
-	# sudo -A apt install gnome-shell-extension-zorin-dash
-	
+	echo -e "${red}Make sure to fill in your password in the Terminal! there is no popup.${reset}"
+	sudo apt install gnome-shell-extension-zorin-dash -y
+	echo -e "${green}Gnome will be restarted in 5 seconds...${reset}"
+	bustctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting...")'
+	echo "waiting 10s..."
+	sleep 10s
+	gnome-extensions disable zorin-hide-activities-move-clock@zorinos.com
+	gnome-extensions disable zorin-menu@zorinos.com
+	gnome-extensions disable zorin-taskbar@zorinos.com
+	gnome-extensions enable zorin-dash@zorinos.com
+	curl https://raw.githubusercontent.com/TGRush/Zorin-extra-Layouts/main/ubuntu-zorin-dash-conf | dconf load /org/gnome/shell/extensions/zorin-dash/
 fi
 else
 echo -e "${red}Zenity not found! run sudo apt install zenity first!${reset}"
